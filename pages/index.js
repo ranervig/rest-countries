524c0bea0a30b98b73b545feacaf426674cf51fc
Rob Nervig <ranervig@gmail.com>
2022-03-18 14:02:35 -0500

    reduced page data size

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from "next/head";
 import { useState, useEffect } from "react/cjs/react.development";
 import Card from "../components/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/Home.module.css";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://restcountries.com/v3.1/all");
   const data = await res.json();
+  //eliminate unnecessary data for page load
+  const countries = data.map(country => {
+    return {
+      name: country.name,
+      cca3: country.cca3,
+      region: country.region,
+      flags: country.flags,
+      population: country.population,
+      capital: country.capital ? country.capital : null,
+    };
+  });
 
   return {
-    props: { countries: data },
+    props: { countries },
   };
 };
 
@@ -36,9 +49,22 @@ export default function Home({ countries }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.input}>
-        <input type={"search"}></input>
+        <div className={styles.search}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
+          <input
+            type={"search"}
+            list={"country list"}
+            placeholder={"Search for a country..."}
+          />
+          <datalist id={"country list"}>
+            {countries.map(country => (
+              <option value={country.name.common} key={country.cca3} />
+            ))}
+          </datalist>
+        </div>
+
         <select onChange={chooseRegion} className={styles.region}>
-          <option value={"All"}>All</option>
+          <option value={"All"}>Filter by region</option>
           <option value={"Africa"}>Africa</option>
           <option value={"Americas"}>Americas</option>
           <option value={"Asia"}>Asia</option>
