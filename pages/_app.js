cba4062e758cc154baab546f3695dc43373144dc
Rob Nervig <ranervig@gmail.com>
2022-03-23 13:03:57 -0500

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
