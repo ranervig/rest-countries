1a47593643b3237560bf51cb9836e6894514a708
Rob Nervig <ranervig@gmail.com>
2022-04-12 15:43:37 -0500

    consistant styling on select

--- pages/index.js
+++ pages/index.js
@@ -75,14 +75,16 @@ export default function Home({ countries }) {
           />
         </div>
 
-        <select onChange={chooseRegion} className={styles.region}>
-          <option value={"All"}>Filter by region</option>
-          <option value={"Africa"}>Africa</option>
-          <option value={"Americas"}>Americas</option>
-          <option value={"Asia"}>Asia</option>
-          <option value={"Europe"}>Europe</option>
-          <option value={"Oceania"}>Oceania</option>
-        </select>
+        <div className={styles.regionSelect}>
+          <select onChange={chooseRegion} className={styles.region}>
+            <option value={"All"}>Filter by region</option>
+            <option value={"Africa"}>Africa</option>
+            <option value={"Americas"}>Americas</option>
+            <option value={"Asia"}>Asia</option>
+            <option value={"Europe"}>Europe</option>
+            <option value={"Oceania"}>Oceania</option>
+          </select>
+        </div>
       </div>
       {displayCountries.length > 0 ? (
         displayCountries.map(country => (
